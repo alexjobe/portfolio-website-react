9f3f48ee152fa70e33f11273404fc25fbb22a68d
Alex Jobe <alex@alexjobe.net>
2020-02-09 13:53:50 -0600

    Add tutorial for Minesweeper

--- src/minesweeper/MessageDisplay.js
+++ src/minesweeper/MessageDisplay.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TutorialImg from '../assets/img/tutorial.jpg';
 
 const gameStates = {
 	GAME_STATE_START: "GAME_STATE_START",
@@ -9,12 +10,22 @@ const gameStates = {
 
 class MessageDisplay extends React.Component {
 
+	state = {
+		showTutorial: false
+	}
+
+	toggleTutorial = () => {
+		this.setState(st => {
+			return { showTutorial: !this.state.showTutorial };
+		});
+	}
+
 	renderMessage = () => {
 		let message = '';
 		switch (this.props.gameState) 
 		{
 			case gameStates.GAME_STATE_PLAY:
-				message = <span>Find the bombs!</span>;
+				message = <span>Find the mines!</span>;
 				break;
 			case gameStates.GAME_STATE_START:
 				message = <span>Click a tile to begin</span>;
@@ -28,15 +39,43 @@ class MessageDisplay extends React.Component {
 			default:
 					// Do nothing
 		}
-		return message;
+		return <div className='message'>{message} <i className="toggle fas fa-question-circle" 
+			onClick={this.toggleTutorial}></i></div>;
+	}
+
+	renderMessageDisplay = () => {
+		let messageClass =  'message-display';
+		if(this.state.showTutorial) { messageClass += ' tutorial'}
+		return (
+			<div className={messageClass}>
+				{	
+					this.state.showTutorial ? this.renderTutorial()
+					:
+					this.renderMessage()
+				}
+			</div>
+		)
+	}	
+
+	renderTutorial = () => {
+		return (
+			<div className="tutorial">
+				<h3>How to Play</h3>
+				<img src={TutorialImg} alt="Tutorial" />
+				<p>
+					Your objective is to clear the board of hidden mines ( <i className="fas fa-radiation"></i> ). The 
+					number on a tile tells you how many mines are adjacent to it. Click a tile to reveal its contents — if 
+					it contains a mine, you lose. The first square you click will never contain a mine. Mark tiles that you 
+					know contain mines by enabling Flag Mode ( <i className="fas fa-flag"></i> ) and clicking them. Once all 
+					empty tiles are revealed, you win!
+				</p>
+				<i className="toggle fas fa-arrow-circle-left" onClick={this.toggleTutorial}></i>
+			</div>
+		);
 	}
 
 	render = () => {
-		return (
-			<div className="message-display">
-				{this.renderMessage()}
-			</div>
-		);
+		return this.renderMessageDisplay();
 	}
 }
 
--- src/portfolio/AboutText.js
+++ src/portfolio/AboutText.js
@@ -10,10 +10,11 @@ const AboutText = () => (
     </p>
     <br />
     <p>
-      I recently completed my double major in <strong>Applied Mathematics::Computer Science</strong> and <strong>Game Design & Development</strong>, but that doesn't mean
-      I'm done learning! Much of my free time is spent at coffee shops and libraries, following lessons on Udemy and Codeacademy,
-      reading books like <em><a target="_blank" rel="noopener noreferrer" href ='https://www.amazon.com/Clean-Code-Handbook-
-      Software-Craftsmanship/dp/0132350882'>Clean Code</a></em> and <em><a target="_blank" rel="noopener noreferrer" href=
+      I recently completed my double major in <strong>Applied Mathematics::Computer Science</strong> and <strong>
+      Game Design & Development</strong>, but that doesn't mean I'm done learning! Much of my free time is spent at coffee 
+      shops and libraries, following lessons on Udemy and Codeacademy, reading books like <em><a target="_blank" 
+      rel="noopener noreferrer" href ='https://www.amazon.com/Clean-Code-Handbook-Software-Craftsmanship/dp/0132350882'>
+      Clean Code</a></em> and <em><a target="_blank" rel="noopener noreferrer" href=
       'https://www.amazon.com/Game-Programming-Patterns-Robert-Nystrom/dp/0990582906'>Game Programming Patterns</a></em>, 
       and challenging myself with new projects.
     </p>
